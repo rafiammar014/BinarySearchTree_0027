b5154133175c2ce340e5f0f60e8c1bddaed61920
rafiammar014 <rafiammar014@gmail.com>
2025-06-26 11:31:59 +0700

    Perbaiki fungsi preorder traversal dan kosong

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -83,6 +83,13 @@ class BinaryTree {
         inorder(ptr->leftchild);
         cout << ptr->info << " ";
         inorder(ptr->rightchild);
-        
+    }
+    void preorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty." << endl;
+            return;
+        }
     }
  }
